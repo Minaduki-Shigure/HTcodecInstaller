0af0002e75d75ff1e4ee2cbab794105b6c8a6e9e
吴康正 <wukangzheng991025@foxmail.com>
2018-07-01 00:29:21 +0800

    Release 2.0

--- HTcodecInstaller/Installer.cpp
+++ HTcodecInstaller/Installer.cpp
@@ -5,24 +5,24 @@
 
 int main(void)
 {
-	printf("默认安装路径是E:\\HTcodec\n");
-	if (_mkdir("E:\\HTcodec"))
+	printf("默认安装路径是D:\\HTcodec\n");
+	if (_mkdir("D:\\HTcodec"))
 	{
-		printf("E:存在HTcodec文件夹是否继续安装？（Y、N）");
+		printf("D:存在HTcodec文件夹是否继续安装？（Y、N）");
 		char c;
 		c = getchar();
 		if (c == 'N' || c == 'n')
 			return 0;
 	}
-	if (!CopyFile(".\\main.exe", "E:\\HTcodec\\main.exe", FALSE))
+	if (!CopyFile(".\\main.exe", "D:\\HTcodec\\main.exe", FALSE))
 		printf("安装失败！\n");
-//	if (!CopyFile(".\\main.o", "E:\\HTcodec\\main.o", FALSE))
+//	if (!CopyFile(".\\main.o", "D:\\HTcodec\\main.o", FALSE))
 //		printf("安装失败！\n");
-	if (!CopyFile(".\\huaji.ico", "E:\\HTcodec\\huaji.ico", FALSE))
+	if (!CopyFile(".\\huaji.ico", "D:\\HTcodec\\huaji.ico", FALSE))
 		printf("安装失败！\n");
-	if (!CopyFile(".\\Install.reg", "E:\\HTcodec\\Install.reg", FALSE))
+	if (!CopyFile(".\\Install.reg", "D:\\HTcodec\\Install.reg", FALSE))
 		printf("安装失败！\n");
-	if (!system("E:\\HTcodec\\Install.reg"))
+	if (!system("D:\\HTcodec\\Install.reg"))
 		printf("\n安装成功！\n\n\n");
 	else
 		printf("安装失败！");
